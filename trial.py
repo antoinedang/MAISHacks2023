1a82679f83c7ad9d0ed1bbdfad89f07c78183d4e
Elie-Dimitri Abdo <90018206+elieDabdo@users.noreply.github.com>
2023-09-30 15:27:30 -0400

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,11 @@
+import magenta.music as mm
+
+# Load a MIDI file
+midi_file_path = 'path/to/your/file.mid'
+midi_sequence = mm.midi_file_to_note_sequence(midi_file_path)
+
+for note in midi_sequence.notes:
+    print(f"Note: {note.pitch}, Start Time: {note.start_time}, End Time: {note.end_time}, Velocity: {note.velocity}")
+
+output_midi_file_path = 'path/to/output/file.mid'
+mm.note_sequence_to_midi_file(midi_sequence, output_midi_file_path)
