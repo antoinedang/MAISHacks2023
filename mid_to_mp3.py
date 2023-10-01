ca9092ecf1f98d8d9e638b5f973de42bc82de10e
frompotenza <potenza.fontes@gmail.com>
2023-10-01 03:51:04 -0400

    front-end improve

--- mid_to_mp3.py
+++ mid_to_mp3.py
@@ -1,27 +1,12 @@
-import mido
-from pydub import AudioSegment
-import subprocess
-import time
+from midi2audio import FluidSynth
+import os
 
-# MIDI file input and output paths
-midi_file = "ItalianconteT1000_Sotto_le_stelle_del_jazz_Conte1.mid"
-output_mp3_file = "output.mp3"
 
-# Initialize FluidSynth with a default SoundFont
-fluidsynth_command = ["fluidsynth", "-a", "alsa", "--audio-driver=alsa", "--gain=0.5", "--reverb=0", "--chorus=0", "/usr/share/soundfonts/default.sf2"]
-fluidsynth_process = subprocess.Popen(fluidsynth_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+songs = ["ItalianconteT1000_Sotto_le_stelle_del_jazz_Conte1"]
 
-# Give FluidSynth some time to initialize
-time.sleep(1)
-
-# Convert MIDI to WAV using FluidSynth
-subprocess.run(["fluidsynth", "-T", "wav", "/usr/share/soundfonts/default.sf2", midi_file])
-
-# Load the generated WAV file
-midi_audio = AudioSegment.from_wav(midi_file.replace('.mid', '.wav'))
-
-# Export the audio as MP3
-midi_audio.export(output_mp3_file, format="mp3")
-
-# Clean up
-fluidsynth_process.terminate()
+for s in songs:
+    # using the default sound font in 44100 Hz sample rate
+    fs = FluidSynth()
+    path = os.path.join(os.getcwd(), s + ".mid")
+    print(path)
+    fs.midi_to_audio(path, s + ".wav")
