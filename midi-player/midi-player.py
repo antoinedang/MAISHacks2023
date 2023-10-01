f2b88ae149071b298aabd386eb3e7e602ba586b6
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-10-01 03:59:02 -0400

    midi player ready

--- midi-player/midi-player.py
+++ midi-player/midi-player.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
 import pygame
+import mido
+from mido import MidiFile, MidiTrack
+import random
 
 app = Flask(__name__)
 
 midi_file = "input.mid"
+drumMidiFilenameList = ["drum1.mid", "drum2.mid"]
 
 @app.route('/play', methods=['POST'])
 def play_midi():
     try:
         pygame.mixer.music.stop()
+        createMelodyWithBeat()
         pygame.mixer.music.load(midi_filename)
         pygame.mixer.music.play()
         
@@ -31,6 +36,32 @@ def pause_midi():
 @app.route('/unpause', methods=['POST'])
 def unpause_midi():
     pygame.mixer.music.unpause()
+    
+def createMelodyWithBeat():
+    # Load the piano MIDI file
+    piano_file = MidiFile(midi_file)
+
+    random_drum_midi = random.sample(drumMidiFilenameList, 1)
+    # Load the drum MIDI file
+    drum_file = MidiFile(random_drum_midi)
+
+    # Create a new MIDI file for the merged tracks
+    merged_file = MidiFile()
+
+    # Create a new track for the merged file
+    merged_track = MidiTrack()
+    merged_file.tracks.append(merged_track)
+
+    # Iterate through the piano file's tracks and add them to the merged track
+    for track in piano_file.tracks:
+        merged_track.extend(track)
+
+    # Iterate through the drum file's tracks and add them to the merged track
+    for track in drum_file.tracks:
+        merged_track.extend(track)
+
+    # Save the merged MIDI file
+    merged_file.save('merged.mid')
 
 if __name__ == '__main__':
     pygame.init()
