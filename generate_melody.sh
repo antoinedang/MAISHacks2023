78c5569214fad604101b8c1d207faa81b610a61d
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-09-30 21:59:10 -0400

    temp

--- generate_melody.sh
+++ generate_melody.sh
@@ -1,10 +1,7 @@
-BUNDLE_PATH=<absolute path of .mag file>
-CONFIG=<one of 'performance', 'performance_with_dynamics', etc., matching the bundle>
-
 performance_rnn_generate \
-    --config=${CONFIG} \
-    --bundle_file=${BUNDLE_PATH} \
-    --output_dir=./generated/melodies \
-    --num_outputs=10 \
-    --num_steps=3000 \
-    --primer_melody="[60,62,64,65,67,69,71,72]"
+--run_dir=/tmp/performance_rnn/logdir/run1 \
+--output_dir=./generated/melodies/ \
+--config=performance_with_dynamics \
+--num_outputs=10 \
+--num_steps=128 \
+--primer_melody="[60,62,64,65,67,69,71,72]"
--- train.sh
+++ train.sh
@@ -4,28 +4,33 @@ read INPUT_DIRECTORY
 echo "Drums or Melodies?"
 read selection
 
+echo "Process MIDI data? [y/n]"
+read process_data
+
 if [ "$selection" = "Drums" ]; then
-  # TFRecord file that will contain NoteSequence protocol buffers.
-  SEQUENCES_TFRECORD=/tmp/drumsequences.tfrecord
+  if [ "$process_data" = "y" ]; then
+    # TFRecord file that will contain NoteSequence protocol buffers.
+    SEQUENCES_TFRECORD=/tmp/drumsequences.tfrecord
 
-  echo "================================"
-  echo "        Processing data..."
-  echo "================================"
+    echo "================================"
+    echo "        Processing data..."
+    echo "================================"
 
-  convert_dir_to_note_sequences \
-    --input_dir=$INPUT_DIRECTORY \
-    --output_file=$SEQUENCES_TFRECORD \
-    --recursive
+    convert_dir_to_note_sequences \
+      --input_dir=$INPUT_DIRECTORY \
+      --output_file=$SEQUENCES_TFRECORD \
+      --recursive
 
-  echo "================================"
-  echo "        Generating dataset..."
-  echo "================================"
+    echo "================================"
+    echo "        Generating dataset..."
+    echo "================================"
 
-  drums_rnn_create_dataset \
-    --config=drum_kit \
-    --input=$SEQUENCES_TFRECORD \
-    --output_dir=/tmp/drums_rnn/sequence_examples \
-    --eval_ratio=0.10
+    drums_rnn_create_dataset \
+      --config=drum_kit \
+      --input=$SEQUENCES_TFRECORD \
+      --output_dir=/tmp/drums_rnn/sequence_examples \
+      --eval_ratio=0.10
+  fi
 
   echo "================================"
   echo "        Training..."
@@ -39,27 +44,28 @@ if [ "$selection" = "Drums" ]; then
   --num_training_steps=20000
 
 elif [ "$selection" = "Melody" ]; then
+  if [ "$process_data" = "y" ]; then
+    SEQUENCES_TFRECORD=/tmp/notesequences.tfrecord
 
-  SEQUENCES_TFRECORD=/tmp/notesequences.tfrecord
+    echo "================================"
+    echo "        Processing data..."
+    echo "================================"
 
-  echo "================================"
-  echo "        Processing data..."
-  echo "================================"
+    convert_dir_to_note_sequences \
+      --input_dir=$INPUT_DIRECTORY \
+      --output_file=$SEQUENCES_TFRECORD \
+      --recursive
 
-  convert_dir_to_note_sequences \
-    --input_dir=$INPUT_DIRECTORY \
-    --output_file=$SEQUENCES_TFRECORD \
-    --recursive
+    echo "================================"
+    echo "        Generating dataset..."
+    echo "================================"
 
-  echo "================================"
-  echo "        Generating dataset..."
-  echo "================================"
-
-  performance_rnn_create_dataset \
-    --config=performance_with_dynamics \
-    --input=$SEQUENCES_TFRECORD \
-    --output_dir=/tmp/performance_rnn/sequence_examples \
-    --eval_ratio=0.10
+    performance_rnn_create_dataset \
+      --config=performance_with_dynamics \
+      --input=$SEQUENCES_TFRECORD \
+      --output_dir=/tmp/performance_rnn/sequence_examples \
+      --eval_ratio=0.10
+  fi
 
   echo "================================"
   echo "        Training..."
@@ -68,7 +74,9 @@ elif [ "$selection" = "Melody" ]; then
   performance_rnn_train \
     --config=performance_with_dynamics \
     --run_dir=/tmp/performance_rnn/logdir/run1 \
-    --sequence_example_file=/tmp/performance_rnn/sequence_examples/training_performances.tfrecord
+    --sequence_example_file=/tmp/performance_rnn/sequence_examples/training_performances.tfrecord \
+    --hparams="batch_size=128,rnn_layer_sizes=[256,256,256]" \
+    --num_training_steps=20000
 
 else
   echo "Invalid input. Please enter 'Drums' or 'Melody'."
