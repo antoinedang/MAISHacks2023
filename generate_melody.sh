64ec8b8a11e26350ac8d723696e4ac54474cef72
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-09-30 22:18:50 -0400

    good

--- generate_melody.sh
+++ generate_melody.sh
@@ -1,7 +1,10 @@
 performance_rnn_generate \
---run_dir=/tmp/performance_rnn/logdir/run1 \
---output_dir=./generated/melodies/ \
---config=performance_with_dynamics \
---num_outputs=10 \
---num_steps=128 \
---primer_melody="[60,62,64,65,67,69,71,72]"
+    --run_dir=/tmp/performance_rnn/logdir/run1 \
+    --output_dir=/tmp/performance_rnn/generated \
+    --config=performance_with_dynamics \
+    --num_outputs=10 \
+    --num_steps=3000 \
+    --primer_melody="[60,62,64,65,67,69,71,72]"
+    # --hparams="batch_size=128,rnn_layer_sizes=[256,256,256]" \
+
+sudo cp /tmp/performance_rnn/generated/* ./generated/melodies/
