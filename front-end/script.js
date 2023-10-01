dd0fce8e52a8fb968d235109a967495036dcd28d
frompotenza <potenza.fontes@gmail.com>
2023-10-01 09:06:06 -0400

    add new button

--- front-end/script.js
+++ front-end/script.js
@@ -7,71 +7,82 @@ const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
+
 // Songs Titles
 const songs = ["lofi_1", "lofi_2"];
+
 // KeepTrack of song
 let songIndex = 0;
+
 // Initially load song details into DOM
 loadSong(songs[songIndex]);
+
 // Update song details
 function loadSong(song) {
-title.innerText = song;
-audio.src = `./music/${song}.mp3`;
-cover.src = `./images/${song}.jpg`;
+  title.innerText = song;
+  audio.src = `./music/${song}.mp3`;
+  cover.src = `./images/${song}.jpg`;
 }
+
 // Play Song
 function playSong() {
-musicContainer.classList.add("play");
-playBtn.querySelector("i.fa").classList.remove("fa-play");
-playBtn.querySelector("i.fa").classList.add("fa-pause");
-audio.play();
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fa").classList.remove("fa-play");
+  playBtn.querySelector("i.fa").classList.add("fa-pause");
+  audio.play();
 }
+
 // Pause Song
 function pauseSong() {
-musicContainer.classList.remove("play");
-playBtn.querySelector("i.fa").classList.add("fa-play");
-playBtn.querySelector("i.fa").classList.remove("fa-pause");
-audio.pause();
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fa").classList.add("fa-play");
+  playBtn.querySelector("i.fa").classList.remove("fa-pause");
+  audio.pause();
 }
+
 // Previous Song
 function prevSong() {
-songIndex--;
-if (songIndex < 0) {
-songIndex = songs.length - 1;
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
 }
-loadSong(songs[songIndex]);
-playSong();
-}
+
 // Next Song
 function nextSong() {
-songIndex++;
-if (songIndex > songs.length - 1) {
-songIndex = 0;
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
 }
-loadSong(songs[songIndex]);
-playSong();
-}
+
 // Update Progress bar
 function updateProgress(e) {
-const { duration, currentTime } = e.srcElement;
-const progressPerCent = (currentTime / duration) * 100;
-progress.style.width = `${progressPerCent}%`;
+  const { duration, currentTime } = e.srcElement;
+  const progressPerCent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPerCent}%`;
 }
+
 // Set Progress
 function setProgress(e) {
-const width = this.clientWidth;
-const clickX = e.offsetX;
-const duration = audio.duration;
-audio.currentTime = (clickX / width) * duration;
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
 }
+
 // Event Listeners
 playBtn.addEventListener("click", () => {
-const isPlaying = musicContainer.classList.contains("play");
-if (isPlaying) {
-pauseSong();
-} else {
-playSong();
-}
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
 // Change Song
 prevBtn.addEventListener("click", prevSong);
