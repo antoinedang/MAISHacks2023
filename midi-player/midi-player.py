0be785393806845298cac2fe8649c6a4c7559102
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-10-01 11:58:30 -0400

    update

--- midi-player/midi-player.py
+++ midi-player/midi-player.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 midi_file = "input.mid"
-drumMidiFilenameList = ["dum_midis/drums1.mid"]
+drumMidiFilenameList = ["actual_drums_1.mid", "actual_drums_2.mid", "actual_drums_3.mid"]
 
 @app.route('/play', methods=['GET'])
 def play_midi():
@@ -40,45 +40,35 @@ def pause_midi():
     paused = not paused
     return jsonify({'result': "success", "message": ""}), 200
     
-def merge_midi_files():
-    # Load the MIDI files
-    midi1 = MidiFile(midi_file)
-    index = int(random.random()*len(drumMidiFilenameList))
-    drums = drumMidiFilenameList[index]
-    midi2 = MidiFile(drums)
+def createMelodyWithBeat():
+    try:
+        # Load the piano MIDI file
+        piano_file = MidiFile(midi_file)
 
-    # midi1.tracks = [track for track in midi1.tracks if not any(msg.type == 'set_tempo' for msg in track)]
-    # midi2.tracks = [track for track in midi2.tracks if not any(msg.type == 'set_tempo' for msg in track)]
+        random_drum_midi = "drum_midis/" + random.sample(drumMidiFilenameList, 1)[0]
+        print(random_drum_midi)
+        # Load the drum MIDI file
+        drum_file = MidiFile(random_drum_midi)
 
-    # Create a new MIDI file for the merged output
-    merged_midi = MidiFile()
-    # append_tracks(midi1, merged_midi, False)
-    # append_tracks(midi2, merged_midi, True, bpm=30)
+        # Create a new MIDI file for the merged tracks
+        merged_file = MidiFile()
 
-    for track in midi1.tracks:
-        new_track = MidiTrack()
-        for msg in track:
-            if not msg.is_meta:
-                new_msg = msg.copy()
-                # new_msg.time *= 2.5
-                # new_msg.time = int(new_msg.time)
-                new_track.append(new_msg)
-        merged_midi.tracks.append(new_track)
+        # Create a new track for the merged file
+        merged_track = MidiTrack()
+        merged_file.tracks.append(merged_track)
 
+        # Iterate through the piano file's tracks and add them to the merged track
+        for track in piano_file.tracks:
+            merged_track.extend(track)
 
-    for track in midi2.tracks:
-        new_track = MidiTrack()
-        # program_change = Message('program_change', channel=0, program=89)
-        # new_track.append(program_change)
-        for msg in track:
-            if msg.type == "set_tempo":
-                new_tempo = mido.bpm2tempo(60)
-                new_track.append(mido.MetaMessage('set_tempo', tempo=new_tempo, time=msg.time))
-            else:
-                new_msg = msg.copy()
-                new_track.append(new_msg)
-        merged_midi.tracks.append(new_track)
-    merged_midi.save("merged.mid")
+        # Iterate through the drum file's tracks and add them to the merged track
+        for track in drum_file.tracks:
+            merged_track.extend(track)
+
+        # Save the merged MIDI file
+        merged_file.save('merged.mid')
+    except Exception as e:
+        print(str(e))
 
 if __name__ == '__main__':
     pygame.init()
